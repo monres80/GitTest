86b05f59c0aa06b91544546248ffbe11fb10b2e5
Pechkov Anton <monres80@gmail.com>
2022-12-26 02:36:54 +0300

    Добавил класс Test1.java

---
+++ src/ru/monresapp/test1/Test1.java
@@ -0,0 +1,12 @@
+package ru.monresapp.test1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1(){
+        System.out.println("Тест 1");
+    }
+}
+
